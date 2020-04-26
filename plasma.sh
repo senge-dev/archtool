5b7398f28c2297b3bd4813f5d4cd85bd8cc059a4
senge6666 <54741446+senge6666@users.noreply.github.com>
2020-04-26 14:44:24 +0800

    Update plasma.sh

--- plasma.sh
+++ plasma.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pacman -S plasma dolphin konsole kde-applications plasma-nm kde-connect
+pacman -S xorg plasma dolphin konsole kde-applications plasma-nm kde-connect
 systemctl enable sddm
 systemctl enable NetworkManager
